98922e40bebb5734ccc584b9895375c50d41f90c
ZoranSudarS48-2020 <zoran.sudar@outlook.com>
2020-10-08 11:06:35 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>WSIT domaci zadatak</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="WSIT domaci zadatak">
+    <meta name="keywords" content="WSIT domaci zadatak">
+    <meta name="author" content="Zoran Sudar">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/main.css" rel="stylesheet">
+</head>
+    <body>
+        <?php 
+        echo "<p>Zoran</p>";
+        echo "<p>Sudar</p>";
+        echo "<p>S48/2020</p>";
+        ?> 
+    </body>
+</html>
+
+
+
+
